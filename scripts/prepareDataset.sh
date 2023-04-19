bba905ccfe1ad36ee12512269dd8eeab601cab50
ichlubna <ichlubna@fit.vutbr.cz>
2023-04-19 12:40:38 +0200

    Scripts

--- scripts/prepareDataset.sh
+++ scripts/prepareDataset.sh
@@ -17,8 +17,7 @@ do
     ID=$((ID+1))
 done
 
-#ffmpeg -i $TEMP"/%05d."$EXT -c:v libaom-av1 -crf 0 -y $2.mkv
-ffmpeg -i $TEMP"/%05d."$EXT -c:v libx264 -y $2.mkv
+ffmpeg -i $TEMP"/%05d."$EXT -c:v libaom-av1 -crf 0 -aom-params lossless=1 -y $2.mkv
 
 R=$3
 C1=0
@@ -31,6 +30,6 @@ C4=$((COUNT-1))
 C4=$(printf $TEMP"/%05d."$EXT $C4)
 
 convert \( $C1 $C2 +append \) \( $C3 $C4 +append \) -append -resize 640x $2"_thumb.jpg"
-montage $1"/*."$EXT -tile $3"x"$3 -geometry 256x144+0+0 $2"_fullThumb.jpg"
+#montage $1"/*."$EXT -tile $3"x"$3 -geometry 256x144+0+0 $2"_fullThumb.jpg"
 
 rm -rf $TEMP
